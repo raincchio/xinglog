d7481f544e1739d0681b16c7fdc93890bcab19ff
raincchio <raincchio@gmail.com>
2025-07-24 15:07:39 +0800

    Update logger.py

--- log/logger.py
+++ log/logger.py
@@ -14,11 +14,11 @@ class Logger:
         self.writer = open(os.path.join(data_dir, datafile_name), 'w')
 
         # Prepare log content headers
-        log_content = headline
+        log_content = ",".join(headline)
         
         self.content_len =len(headline)
         # Write headers to the log file
-        self.writer.write(",".join(log_content) + '\n')
+        self.writer.write(log_content + '\n')
 
     def log(self, variable_value):
         '''
@@ -26,8 +26,10 @@ class Logger:
         variable_value: [0.1,0.2,0.3]
         '''
         assert len(variable_value)==self.content_len
+
+        log_content = ",".join(variable_value)
             
-        self.writer.write(str(variable_value) + '\n')
+        self.writer.write(str(log_content) + '\n')
  
 
     def close(self):
