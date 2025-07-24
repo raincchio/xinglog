6b0776d7f4e7804c1a9a514193ff881d9c56fb5f
raincchio <raincchio@gmail.com>
2025-07-24 14:52:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='log_writer_package',
+    name='xinglog',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
      # os is part of the Python standard library, so it's just an example
     ],
-    description='A package for logging model training information',
+    description='A package for logging training information',
     author='raincchio',
     author_email='raincchio@gmail.com',
     url='https://github.com/raincchio/xinglog',  # Replace with your actual URL
